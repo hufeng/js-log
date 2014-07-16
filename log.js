8ff4d9307309d2260438dc2dfb5dc30e4588ec12
胡锋(of730) <hufeng@qianmi.com>
2014-07-16 13:51:54 +0800

    实现log.js

---
+++ log.js
@@ -0,0 +1,15 @@
+/**
+ * log.js hufeng
+ * The safe wrapper for `console.xxx` functions
+ *  log("message") ==> console.log("message")
+ *  log("message", "warn") ==> console.warn("message")
+ */
+module.exports = function() {
+	var isDebug = parent.window.location.hash === '#debug';
+	
+	window.console &&
+	// Do NOT print `log(msg)` in non-debug mode
+	isDebug &&
+	// Call native method of console
+	console.log.apply(console, arguments);
+}
